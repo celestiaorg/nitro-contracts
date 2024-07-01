f57c05b4096ca71e43c96381160a2e809da461a7
Diego <diego@celestia.org>
2024-07-01 18:57:20 -0400

    0.8.19 compatibility

--- src/celestia/IBlobstreamX.sol
+++ src/celestia/IBlobstreamX.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.19;
 
 import "lib/blobstream-contracts/src/DataRootTuple.sol";
 import "lib/blobstream-contracts/src/lib/tree/binary/BinaryMerkleProof.sol";
